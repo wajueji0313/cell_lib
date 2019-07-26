2ad0de8ee466637190e3838a88e1ae4f6bf7320c
liuxf <2212289801@qq.com>
2019-07-27 05:59:53 +0800

    add a reg wq

--- test.v
+++ test.v
@@ -1,6 +1,14 @@
 module test (
 
 );
-dsafasdgasgasgasgsagsa`1
+
+reg  [7 :0]  test_cnt  ;
+always @ (posedge mclk or negedge mreset_n)
+begin
+    if (!mreset_n)
+        test_cnt          <=  8'b0 ;
+    else 
+        test_cnt          <=  test_cnt + 8'b1 ;
+end
 
 endmodule
